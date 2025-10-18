9cfdbefde0ff71cd28a9e60e5b485d86824cae87
Linara Sankubaeva <linarasankubaeva55@mail.ru>
2025-10-18 15:48:40 +0300

    Fixed the problem where lower bound was always equal to upper bound because of lack of curly brackets after the if statements.

--- src/main.c
+++ src/main.c
@@ -8,12 +8,14 @@ int main(int argc, char *argv[]) {
 	int lower_bound_flag = 0, upper_bound_flag = 0;
 
 	for (int i = 0; i < argc; i++) {
-		if (strncmp(argv[i], "--lower_bound=", 14) == 0)
+		if (strncmp(argv[i], "--lower_bound=", 14) == 0) {
 			lower_bound_flag = 1;
 			sscanf(argv[i] + 14, "%d", &lower_bound);
-		if (strncmp(argv[i], "--upper_bound=", 14) == 0)
+		}
+		if (strncmp(argv[i], "--upper_bound=", 14) == 0) {
 			upper_bound_flag = 1;
 			sscanf(argv[i] + 14, "%d", &upper_bound);
+		}
 	}
 
 	if (!lower_bound_flag && !upper_bound_flag) {
